e7ac00c01a2c44c094e52840acb2247fbb66389e
Kerim Halilovic <khalilovic1@etf.unsa.ba>
2024-01-06 12:04:21 +0100

    upiti implementation

--- public/spojiUpite.js
+++ public/spojiUpite.js
@@ -1,7 +1,11 @@
+let loggedInUser = null;
+
 PoziviAjax.getKorisnik(function (error, data) {
     if (error) {
         console.log(error)
     } else {
+        console.log(data);
+        loggedInUser = data;
         const form = document.getElementById("upit_input");
         form.innerHTML = `
         <input type="text" id="tekst_upita">
@@ -14,12 +18,25 @@ function posaljiUpit() {
     const tekstUpita = document.getElementById("tekst_upita").value;
     const urlSearchParams = new URLSearchParams(window.location.search);
     const nekretninaDetails = Object.fromEntries(urlSearchParams.entries());
-    
+
     PoziviAjax.postUpit(nekretninaDetails.id, tekstUpita, function (error, data) {
         if (error) {
             console.log(error)
         } else {
-            console.log(data)
+            if (!loggedInUser) {
+                return;
+            }
+            const upitElement = document.createElement("li");
+            upitElement.innerHTML = `
+            <h4>${loggedInUser.username}</h4>
+            <p>${tekstUpita}</p>
+            `;
+            document.getElementById("upiti-list").appendChild(upitElement);
+            const form = document.getElementById("upit_input");
+            form.innerHTML = `
+        <input type="text" id="tekst_upita">
+        <button id="submit-query-button" onclick="posaljiUpit()">Pošalji upit</button>
+        `;
         }
     });
 }
